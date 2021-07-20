66212fe14d98970d3cf3b44015340b65410718ad
Carlos Ruesta <carlos@dcide.com.br>
2021-07-20 19:34:53 -0300

    Curso3 - Setup do projeto para o curso 3. Mudança nos locais das pastas: APP/SRC/DIST

--- app/src/app.ts
+++ app/src/app.ts
@@ -5,7 +5,6 @@ const negociacaoController = new NegociacaoController();
 const form = document.querySelector('.form');
 
 if (!form) {
-    console.log(1);
     throw Error("Não foi possivel inicializar a aplicação. Falta o formulário");
 }
 
